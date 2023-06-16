e565137469a71071d015b512cd3b16e576b795a3
cmcoke <minoru.cm@gmail.com>
2023-06-16 14:41:34 -0500

    created the layout of the app.

---
+++ components/Header.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { useRouter } from "next/navigation";
+import Button from "./Button";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header = ({ children, className }: HeaderProps) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // handles logging out of application
+  };
+
+  return (
+    <div className={twMerge(` h-fit bg-gradient-to-b from-emerald-800 p-6`, className)}>
+      <div className="w-full mb-4 flex items-center justify-between">
+        {/* back and foward buttons for screens with a width of 768px and more */}
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="
+              rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="
+              rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+
+        {/* home and search buttons for screens with a width less than 769px */}
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            onClick={() => router.push("/")}
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button
+            onClick={() => router.push("/search")}
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+
+        {/* logout, sign up and login button */}
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button className=" bg-transparent text-neutral-300 font-medium" onClick={() => {}}>
+                Sign Up
+              </Button>
+            </div>
+            <div>
+              <Button className=" bg-white px-6 py-2" onClick={() => {}}>
+                Login In
+              </Button>
+            </div>
+          </>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
+export default Header;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar = ({ children }: SidebarProps) => {
+  const pathname = usePathname(); // is a client component hook that get the current URL's pathname
+
+  // creates the different routes (Home and Seach) for the application
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname !== "/search", // when the pathname is not search then the 'Home' route becomes active
+        href: "/"
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathname === "/search", // when the pathname is search then the 'Search' route becomes active
+        href: "/search"
+      }
+    ],
+    [pathname]
+  );
+
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map(item => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
+};
+export default Sidebar;
